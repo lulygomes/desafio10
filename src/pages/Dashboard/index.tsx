593f78e0bd7e79d1cae29c4f310cc868d74e762d
Luiz Silva <luiz.gsilva42@gmail.com>
2020-11-30 14:51:53 -0300

    delete food

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -72,6 +72,11 @@ const Dashboard: React.FC = () => {
 
   async function handleDeleteFood(id: number): Promise<void> {
     // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+
+    const newFoods = foods.filter(f => f.id !== id);
+
+    setFoods([...newFoods]);
   }
 
   function toggleModal(): void {
